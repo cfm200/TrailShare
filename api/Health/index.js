fc72e33acacebcb59255b89656bf22b02f998134
Ciaran <ciaranmahon@hotmail.co.uk>
2026-01-07 11:23:43 +0000

    api health

---
+++ api/Health/index.js
@@ -0,0 +1,11 @@
+module.exports = async function (context, req) {
+    context.res = {
+      status: 200,
+      body: {
+        ok: true,
+        time: new Date().toISOString(),
+        node: process.version
+      }
+    };
+  };
+  
